96373a022ead2707a6536d5428fdd790feaca15e
KarlTamberg <tamberg.karl@gmail.com>
2022-11-23 23:52:25 +0200

    fix path;

--- autorun.sh
+++ autorun.sh
@@ -9,10 +9,10 @@ sudo sed -i "s#CIS_ROOT_DIR=.*#CIS_ROOT_DIR='$(pwd)'#" /etc/default/cis-hardenin
 sudo ./bin/hardening.sh --audit --allow-unsupported-distribution
 sudo ./bin/hardening.sh --apply --set-hardening-level 2 --allow-unsupported-distribution
 sudo ./bin/hardening.sh --apply --allow-unsupported-distribution
-sudo ./custom_scripts/1.1.1.x_unused_filesystems.sh
-sudo ./custom_scripts/1.1.23_disable_usb_storage.sh
-sudo ./custom_scripts/3.4.x_unused_services.sh
-sudo ./custom_scripts/5.1.2_crontab_permissions.sh
+sudo ./../custom_scripts/1.1.1.x_unused_filesystems.sh
+sudo ./../custom_scripts/1.1.23_disable_usb_storage.sh
+sudo ./../custom_scripts/3.4.x_unused_services.sh
+sudo ./../custom_scripts/5.1.2_crontab_permissions.sh
 
 # Audit
 sudo ./bin/hardening.sh --audit --allow-unsupported-distribution
