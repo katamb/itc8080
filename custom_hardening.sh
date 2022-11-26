d09c9dc5a8714dbfc022335b65a9acbabdfdb551
KarlTamberg <tamberg.karl@gmail.com>
2022-11-26 22:58:02 +0200

    fix path;

--- custom_hardening.sh
+++ custom_hardening.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo ./../custom_scripts/1.1.1.1_cramfs.sh
-sudo ./../custom_scripts/1.1.1.2_squashfs.sh
+sudo ./custom_scripts/1.1.1.1_cramfs.sh
+sudo ./custom_scripts/1.1.1.2_squashfs.sh
--- custom_scripts/1.1.1.2_squashfs.sh
+++ custom_scripts/1.1.1.2_squashfs.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 {
   l_output="" l_output2=""
-  l_mname="cramfs" # set module name
+  l_mname="squashfs" # set module name
   # Check if the module exists on the system
   if
-    [ -z "$(
-      modprobe -n -v "$l_mname" 2>&1 | grep -Pi -- "\h*modprobe:\h+FATAL:\h+Module\h+$l_mname\h+not\h+found\h+in\h+directory"
-    )"
-    ]
+    [ -z "$(modprobe -n -v "$l_mname" 2>&1 | grep -Pi -- "\h*modprobe:\h+FATAL:\h+Module\h+$l_mname\h+not\h+found\h+in\h+directory" )" ]
   then
     # Check how module will be loaded
     l_loadable="$(modprobe -n -v "$l_mname")"
-    [ "$(wc -l <<<"$l_loadable")" -gt "1" ] && l_loadable="$(
-      grep -P -- "(^\h*install|\b$l_mname)\b" <<<"$l_loadable"
-    )"
+    [ "$(wc -l <<<"$l_loadable")" -gt "1" ] && l_loadable="$(grep -P -- "(^\h*install|\b$l_mname)\b" <<<"$l_loadable")"
     if grep -Pq -- '^\h*install \/bin\/(true|false)' <<<"$l_loadable"; then
       l_output="$l_output\n - module: \"$l_mname\" is not loadable: \"$l_loadable\""
     else
