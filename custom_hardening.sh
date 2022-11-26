f1c99234b616ee7d2c24b760ef5b094afdb06d88
KarlTamberg <tamberg.karl@gmail.com>
2022-11-26 23:39:56 +0200

    unify script;

--- custom_hardening.sh
+++ custom_hardening.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo ./custom_scripts/1.1.1.x_unused_filesystems.sh
+sudo ./custom_scripts/3.1.x_network_services.sh
--- custom_scripts/1.1.1.x_unused_filesystems.sh
+++ custom_scripts/1.1.1.x_unused_filesystems.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./modprobe_audit_and_fixing.sh
+source $(dirname "$0")/custom_scripts/modprobe_audit_and_fixing.sh
 
 audit_and_fix "cramfs"
 audit_and_fix "squashfs"
