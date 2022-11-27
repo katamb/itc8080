ea12c6bcacb94cba23472852c29a17873cc91067
KarlTamberg <tamberg.karl@gmail.com>
2022-11-27 12:10:15 +0200

    Ufw script;

--- custom_scripts/3.5.1.x_configure_ufw.sh
+++ custom_scripts/3.5.1.x_configure_ufw.sh
@@ -12,7 +12,7 @@ fi
 
 echo "3.5.1.2 Ensure iptables-persistent is not installed with ufw"
 not_installed="package 'iptables-persistent' is not installed and no information is available"
-if [[ "$(dpkg-query -s iptables-persistent)" == *"$not_installed"* ]]; then
+if [[ "$(dpkg-query -s iptables-persistent)" == *"not installed"* ]]; then
   echo -e "\n- Audit Result:\n ** PASS **\niptables-persistent is not installed\n"
 else
   echo -e "\n- Audit Result:\n ** FAIL **\n - Reason(s) for audit failure:\niptables-persistent is installed\n"
@@ -23,28 +23,22 @@ fi
 
 echo "3.5.1.3 Ensure ufw service is enabled"
 if [ "$(systemctl is-enabled ufw.service)" == "enabled" ]; then
-  ufw_enabled="$(true)"
   echo -e "\n- UFW enabled\n"
 else
-  ufw_enabled="$(false)"
   echo -e "\n- UFW not enabled, enabling\n"
   systemctl unmask ufw.service
 fi
 
 if [ "$(systemctl is-active ufw)" == "active" ]; then
-  ufw_daemon_active="$(true)"
   echo -e "\n- UFW daemon active\n"
 else
-  ufw_daemon_active="$(false)"
   echo -e "\n- UFW daemon not active, enabling\n"
   systemctl --now enable ufw.service
 fi
 
 if [ "$(ufw status)" == "Status: active" ]; then
-  ufw_daemon_active="$(true)"
   echo -e "\n- UFW daemon active\n"
 else
-  ufw_daemon_active="$(false)"
   echo -e "\n- UFW daemon not active, enabling\n"
   ufw enable
 fi
