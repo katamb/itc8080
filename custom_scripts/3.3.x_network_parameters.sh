9b06addf3eaddb7b334806de3d1c28507f85800f
KarlTamberg <tamberg.karl@gmail.com>
2022-11-27 13:04:11 +0200

    Ufw script;

--- custom_scripts/3.3.x_network_parameters.sh
+++ custom_scripts/3.3.x_network_parameters.sh
@@ -114,15 +114,19 @@ audit_and_fix() {
   fi
 }
 
+# Currently doesn't pass
 echo "3.3.1 Ensure source routed packets are not accepted"
 audit_and_fix "net.ipv4.conf.all.accept_source_route=0 net.ipv4.conf.default.accept_source_route=0 net.ipv6.conf.all.accept_source_route=0 net.ipv6.conf.default.accept_source_route=0"
 
+# Currently doesn't pass
 echo "3.3.2 Ensure ICMP redirects are not accepted"
 audit_and_fix "net.ipv4.conf.all.accept_redirects=0 net.ipv4.conf.default.accept_redirects=0 net.ipv6.conf.all.accept_redirects=0 net.ipv6.conf.default.accept_redirects=0"
 
+# Currently doesn't pass
 echo "3.3.3 Ensure secure ICMP redirects are not accepted"
 audit_and_fix "net.ipv4.conf.default.secure_redirects=0 net.ipv4.conf.all.secure_redirects=0"
 
+# Currently doesn't pass
 echo "3.3.4 Ensure suspicious packets are logged"
 audit_and_fix "net.ipv4.conf.all.log_martians=1 net.ipv4.conf.default.log_martians=1"
 
@@ -132,11 +136,13 @@ audit_and_fix "net.ipv4.icmp_echo_ignore_broadcasts=1"
 echo "3.3.6 Ensure bogus ICMP responses are ignored"
 audit_and_fix "net.ipv4.icmp_ignore_bogus_error_responses=1"
 
+# Currently doesn't pass
 echo "3.3.7 Ensure Reverse Path Filtering is enabled"
 audit_and_fix "net.ipv4.conf.all.rp_filter=1 net.ipv4.conf.default.rp_filter=1"
 
 echo "3.3.8 Ensure TCP SYN Cookies is enabled"
 audit_and_fix "net.ipv4.tcp_syncookies=1"
 
+# Currently doesn't pass
 echo "3.3.9 Ensure IPv6 router advertisements are not accepted"
 audit_and_fix "net.ipv6.conf.all.accept_ra=0 net.ipv6.conf.default.accept_ra=0"
--- custom_scripts/3.5.1.x_configure_ufw.sh
+++ custom_scripts/3.5.1.x_configure_ufw.sh
@@ -83,7 +83,27 @@ echo "No need in current context"
 echo "3.5.1.6 Ensure ufw firewall rules exist for all open ports"
 ufw_out="$(ufw status verbose)"
 ss -tuln | awk '($5!~/%lo:/ && $5!~/127.0.0.1:/ && $5!~/::1/) {split($5, a,":"); print a[2]}' | sort | uniq | while read -r lpn; do
- ! grep -Pq "^\h*$lpn\b" <<< "$ufw_out" && echo "- Port: \"$lpn\" is missing a firewall rule"
- echo "Adding rule"
- ufw allow in "$lpn"
+  ! grep -Pq "^\h*$lpn\b" <<<"$ufw_out" && echo "- Port: \"$lpn\" is missing a firewall rule"
+  echo "Adding rule"
+  ufw allow in "$lpn"
 done
+
+echo "3.5.1.7 Ensure ufw default deny firewall policy"
+if [[ "$(ufw status verbose | grep Default: | grep -c 'allow')" == 0 ]]; then
+  echo -e "\nufw default deny firewall policy is configured\n"
+else
+  echo -e "\nufw default deny firewall policy is configured - configuring\n"
+
+  ufw allow git
+  ufw allow in http
+  ufw allow out http
+  ufw allow in https
+  ufw allow out https
+  ufw allow out 53
+  ufw logging on
+
+  echo "Remediating"
+  ufw default deny incoming
+  ufw default deny outgoing
+  ufw default deny routed
+fi
