ddecf493bf5eb8c401c463f02b3f8699a455f32f
KarlTamberg <tamberg.karl@gmail.com>
2022-11-27 12:54:52 +0200

    Ufw script;

--- custom_scripts/3.5.1.x_configure_ufw.sh
+++ custom_scripts/3.5.1.x_configure_ufw.sh
@@ -76,3 +76,14 @@ else
   echo "Remediating"
   ufw deny in from ::1
 fi
+
+echo "3.5.1.5 Ensure ufw outbound connections are configured"
+echo "No need in current context"
+
+echo "3.5.1.6 Ensure ufw firewall rules exist for all open ports"
+ufw_out="$(ufw status verbose)"
+ss -tuln | awk '($5!~/%lo:/ && $5!~/127.0.0.1:/ && $5!~/::1/) {split($5, a,":"); print a[2]}' | sort | uniq | while read -r lpn; do
+ ! grep -Pq "^\h*$lpn\b" <<< "$ufw_out" && echo "- Port: \"$lpn\" is missing a firewall rule"
+ echo "Adding rule"
+ ufw allow in "$lpn"
+done
