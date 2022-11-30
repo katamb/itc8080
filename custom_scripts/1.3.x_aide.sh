99db275bbfd88c016c167ed273f40659ebe0bf47
KarlTamberg <tamberg.karl@gmail.com>
2022-11-30 21:22:37 +0200

    Add scrips;

--- custom_scripts/1.3.x_aide.sh
+++ custom_scripts/1.3.x_aide.sh
@@ -26,7 +26,7 @@ echo -e "---\n"
 echo "---"
 echo "1.3.2 Ensure filesystem integrity is regularly checked"
 needs_init=false
-if [ "$(grep -c -Prs '^([^#\n\r]+\h+)?(\/usr\/s?bin\/|^\h*)aide(\.wrapper)?\h+(--check|([^#\n\r]+\h+)?\$AIDEARGS)\b' /etc/cron.* /etc/crontab /var/spool/cron/)" == 1 ]; then
+if [ "$(grep -c -Prs '^([^#\n\r]+\h+)?(\/usr\/s?bin\/|^\h*)aide(\.wrapper)?\h+(--check|([^#\n\r]+\h+)?\$AIDEARGS)\b' /etc/cron.* /etc/crontab /var/spool/cron/)" -gt 0 ]; then
   echo -e "AIDE runs regularly\n"
 else
   echo -e "AIDE cronjob missing, remediating\n"
